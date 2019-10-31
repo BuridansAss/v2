8aaab565121281f2c37691c76deb0e86b75dd902
Evgeniy_Khorsun <e.horsun@playneta.gg>
2019-10-31 20:04:06 +0300

    add codeception

--- app/kiss.php
+++ app/kiss.php
@@ -1,1 +1,26 @@
 <?php
+
+require '../vendor/autoload.php';
+//441 475
+//861 895
+
+//$pictures = range(441, 475);
+//
+//$dpis = ['1x/', '2x/', '3x/', 'hdpi/', 'ldpi/', 'mdpi/', 'xhdpi/', 'xxhdpi/', 'xxxhdpi/'];
+//
+//$big = './stickers/big/';
+//$regular = './stickers/regular/';
+//
+//$i = 861;
+//foreach ($pictures as $picture)
+//{
+//    foreach ($dpis as $dpi) {
+//        copy($big . $dpi . $picture . '.png', $regular . $dpi . $i . '.png');
+//    }
+//
+//    $i++;
+//}
+
+$background = '/home/evgen/Prjs/kiss-tools/app/background/';
+
+\KissTools\Engine\Images\Asshole\TMP\MobileConverter::create('/home/evgen/Prjs/kiss-tools/app/popup.png', $background);
